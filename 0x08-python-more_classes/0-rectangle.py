d45c3875c8ca8dab60d5326a9dbecd4343e76130
Kyuka Ibrahim Avong <drkyavoni@live.com>
2023-11-28 10:20:51 +1300

    "v0.1"

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Module contains class Rectangle
+"""
+
+
+class Rectangle:
+    """
+        attribute: empty
+    """
+    pass
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+    Module contains class Rectangle
+    Attributes:
+        width: getter, setter
+        height: getter, setter
+"""
+
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        """
+            Initialize private width and height
+            attributes:
+                width: int
+                height: int
+            Return: void
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+            Return: __width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+            Validate width as integer and greater than 0
+            Return: void
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+            Return: __height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+            Validate height as integer and greater than 0
+            Return: void
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+            Function calculates the area of a rectangle
+            Return: int
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+            Function calculates the perimeter of a rectangle
+            Return: int
+        """
+        return 2 * (self.__width + self.__height)
