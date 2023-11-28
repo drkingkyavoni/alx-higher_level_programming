94d0f4b9ae733ab54142214e470c107493ff7514
Kyuka Ibrahim Avong <drkyavoni@live.com>
2023-11-29 07:20:39 +1300

    "v0.2.2"

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -92,3 +92,13 @@ class Rectangle:
             self.__width,
             self.__height
         )
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """ Function returns the biggest rectangle based on the area
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
