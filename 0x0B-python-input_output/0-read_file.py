4132dbc23b7a6bc697c575fb7b3e98da2f0e27a3
Kyuka Ibrahim Avong <drkyavoni@live.com>
2023-12-07 15:44:02 +1300

    "v0.1.8.1"

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     """ function that reads a text file
     """
     with open(filename, mode="r", encoding="utf-8") as fname:
-        print(fname.read(), end="\n")
+        print(fname.read())
