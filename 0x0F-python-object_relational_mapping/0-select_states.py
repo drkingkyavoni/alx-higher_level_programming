ea3f6d4edac4f76e0577d77f43377250e8460559
Ibrahim Kyuka Avong <drkyavoni@live.com>
2024-06-25 09:39:06 +1200

    "v0.2.2.1"

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,27 +12,17 @@ def get_states(_usr: str, _pwd: str, _db: str) -> None:
     """
         Function gets state from a db with username and password
     """
-    cursor = ""
-
-    try:
-        # print(f'{_usr}@{_db}.localhost:{_pwd}')
-        conn = MySQLdb.connect(host="localhost", port=3306, user=_usr, passwd=_pwd, db=_db)
-        cursor = conn.cursor()
-        query = "select * from states order by id asc;"
-        cursor.execute(query)
-        result = cursor.fetchall()
-        for row in result:
-            print(row)
-    except MySQLdb.Error as err:
-        print(f"Connection to {_db} failed: {err}")
-    finally:
-        if cursor:
-            cursor.close()
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=_usr, passwd=_pwd, db=_db,
+                           charset="utf8")
+    cursor = conn.cursor()
+    cursor.execute("select * from states order by id asc;")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+    cursor.close()
+    conn.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./script.py username password database")
-        sys.exit(1)
-
     _, _usr, _pwd, _db = tuple(sys.argv)
     get_states(_usr, _pwd, _db)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,27 +13,18 @@ def filter_states(_usr: str, _pwd: str, _db: str) -> None:
         Function filters state with names that start with 'N'
         from a db with username and password
     """
-    cursor = ""
-
-    try:
-        conn = MySQLdb.connect(host="localhost", port=3306, user=_usr, passwd=_pwd, db=_db)
-        cursor = conn.cursor()
-        query = "select * from states where name like 'N%';"
-        cursor.execute(query)
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-    except MySQLdb.Error as err:
-        print(f"Connection failed on {_db}: {err}")
-    finally:
-        if cursor:
-            cursor.close()
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=_usr, passwd=_pwd, db=_db,
+                           charset="utf8")
+    cursor = conn.cursor()
+    cursor.execute("select * from states where name like 'N%';")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    conn.close()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./script.py username password database")
-        sys.exit(1)
-
     _, _usr, _pwd, _db = tuple(sys.argv)
     filter_states(_usr, _pwd, _db)
