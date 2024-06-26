084889bbaff93590261d2c3bf8928260441e5da0
Ibrahim Kyuka Avong <drkyavoni@live.com>
2024-06-27 11:21:39 +1200

    "v0.2.3.1"

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
 
 """
-    Module contains get_states function
-    Returns: None
+Function that filters state with names that start with 'N'
+Returns: None
 """
 
 import sys
-
 import MySQLdb
 
-
-def filter_states(_usr: str, _pwd: str, _db: str) -> None:
+if __name__ == "__main__":
     """
-    Function filters state with names that start with 'N'
+    filters state with names that start with 'N'
     from a db table with username and password
     """
+
     conn = MySQLdb.connect(
-        host="localhost", port=3306, user=_usr, passwd=_pwd, db=_db, charset="utf8"
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8",
     )
+
     cursor = conn.cursor()
-    cursor.execute("select * from states where name like 'N%' order by id asc")
-    results = cursor.fetchall()
+    results = cursor.execute(
+        "select * from states where name like 'N%' order by id asc"
+    ).fetchall()
+
     for row in results:
         print(row)
     cursor.close()
     conn.close()
-
-
-if __name__ == "__main__":
-    _, _usr, _pwd, _db = tuple(sys.argv)
-    filter_states(_usr, _pwd, _db)
