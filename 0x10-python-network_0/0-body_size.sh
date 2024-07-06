0b5bac9bbe70cf9b99430a249b4b7fe91ca5d239
Ibrahim Kyuka Avong <drkyavoni@live.com>
2024-07-06 22:14:11 +1200

    updated to /bin

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Script that takes in a URL, sends a request to that URL, and # displays the size of the body of the response
 curl -s --head $1 | grep -i content-length | awk 'BEGIN {FS=": "}; {print $2}'
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -s -I $1 | grep -i allow | awk 'BEGIN {FS=": "}; {print $2}'
