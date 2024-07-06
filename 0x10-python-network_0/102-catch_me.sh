1353170aad7cf283b6edf39775169ba8289fe0f8
Ibrahim Kyuka Avong <drkyavoni@live.com>
2024-07-07 04:56:18 +1200

    updated advanced to be silent

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -L -d "user_id=98" -H "Origin: School" -X PUT 0.0.0.0:5000/catch_me_2
+curl -sL -d "user_id=98" -H "Origin: School" -X PUT 0.0.0.0:5000/catch_me_2
