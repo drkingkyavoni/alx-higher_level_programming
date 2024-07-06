3849adda15f8a46fe008e6b9493052cf016ebe8c
Ibrahim Kyuka Avong <drkyavoni@live.com>
2024-07-06 22:09:20 +1200

    changed to wc -l file to 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/env bash
-# Script that takes in a URL, sends a request to that URL, and
-# displays the size of the body of the response
-
-if [[ $# -ne 1 ]]; then
-	echo "Usage: ./0-body_size.sh host:port"
-	exit 1
-fi
-# curl -s --head $1 | grep -i content-length | awk -d: '{print $2}'
+#!/usr/bin/bash
+# Script that takes in a URL, sends a request to that URL, and # displays the size of the body of the response
 curl -s --head $1 | grep -i content-length | awk 'BEGIN {FS=": "}; {print $2}'
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,11 +1,3 @@
-#!/usr/bin/env bash
-# Script that takes in a URL, sends a GET request to the URL,
-# and displays the body of the response
-
-if [[ $# -lt 1 ]]; then
-	echo "Usage: ./3-methods.sh host:port"
-	exit 1
-fi
-# echo $1
-# curl -s --head $1 | grep -i allow | awk -d: '{print $2}'
+#!/usr/bin/bash
+# Script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -s -I $1 | grep -i allow | awk 'BEGIN {FS=": "}; {print $2}'
